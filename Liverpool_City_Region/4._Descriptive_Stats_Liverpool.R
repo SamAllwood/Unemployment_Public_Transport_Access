1ca67f3352aedadf0f0a0a597d0bc1e9959f271b
Sam Allwood <sam.allwood3@gmail.com>
2024-10-21 18:00:30 +0100

    Descriptive stats file inital commit

--- Liverpool_City_Region/4._Descriptive_Stats_Liverpool.R
+++ Liverpool_City_Region/4._Descriptive_Stats_Liverpool.R
@@ -1,5 +1,4 @@
 # 1. Setup ----------------------------------------------------------------
-
 library(tidyverse)
 library(knitr)
 library(tidytransit)
@@ -14,20 +13,18 @@ library(kableExtra)
 library(officer)
 library(flextable)
 
-setwd("~/Google Drive/My Drive/MSc Urban Transport/1.Dissertation/Programming")
-
 # 2. Load Data ------------------------------------------------------------
 # Read population density shapefile from "PopulationDensityChange.R" script
-MAN_pop <- read_sf("Data/MANCH_population.shp") %>%
+LCR_pop <- read_sf("Data/LCR_population.shp") %>%
   st_transform(4326) %>%
   rename("Pop_Dens_change" = "Pp_dns_",
          "LSOA_Code" = "LSOA21C",
-         "Man_Pop_Dens"  = "P_2021_",
-         "Man_Pop" = "U_P_202") %>%
-  dplyr::select(LSOA_Code, Pop_Dens_change, Man_Pop_Dens, Man_Pop) %>%
+         "LCR_Pop_Dens"  = "P_2021_",
+         "LCR_Pop" = "U_P_202") %>%
+  dplyr::select(LSOA_Code, Pop_Dens_change, LCR_Pop_Dens, LCR_Pop) %>%
   as.data.frame()
 
-MANCH_dataset_descript <- read_csv("Data/MANCH_dataset_full.csv") %>%
+LCR_dataset_descript <- read_csv("../Data/LCR_dataset_full.csv") %>%
   dplyr::select(-c("Apprent_qual",
             "Level_2_qual", 
             "Level_2_qual", 
@@ -38,30 +35,26 @@ MANCH_dataset_descript <- read_csv("Data/MANCH_dataset_full.csv") %>%
   rename("White_percent" = "% White",
          "Single_parent_household_rate" = "%",
          "Townsuburb" = "twnsbrb") %>%
-  left_join(MAN_pop, by=  "LSOA_Code" ) %>%
-  dplyr::select("LSOA_Code", 
-         "LSOA_Name", 
-         "LAD22CD",
-         "LSOA_Area",
-         "Unemployment_rate",
-         "PT_Job_Access_Index",
-         "Man_Pop_Dens",
-         "Man_Pop",
-         "Townsuburb",
-         "TCITY15",
-         "Employed_Population",
-         "Economically_active",
-         "Unemployed",
-    #     "Traveltime_empcent",
-    #     "TravelTime_Jobcentre",
-         "No_car_rate",
-         "White_percent",
-         "Single_parent_household_rate",
-         "Low_qual_percent",
-         "SEC_Management_pc") %>%
-#         "SEC_Lower_supervisory_routine_pc") %>%
-  mutate(#TravelTime_Jobcentre = as.numeric(TravelTime_Jobcentre),
-         PT_Job_Access_Index = as.numeric(PT_Job_Access_Index)/1000,
+  left_join(LCR_pop, by =  "LSOA_Code") %>%
+  dplyr::select("LSOA_Code",
+                "LSOA_Name",
+                "LAD22CD",
+                "LSOA_Area",
+                "Unemployment_rate",
+                "PT_Job_Access_Index",
+                "LCR_Pop_Dens",
+                "LCR_Pop",
+                "Townsuburb",
+                "TCITY15",
+                "Employed_Population",
+                "Economically_active",
+                "Unemployed",
+                "No_car_rate",
+                "White_percent",
+                "Single_parent_household_rate",
+                "Low_qual_percent",
+                "SEC_Management_pc") %>%
+  mutate(PT_Job_Access_Index = as.numeric(PT_Job_Access_Index) / 1000,
          Unemployment_rate = as.numeric(Unemployment_rate),
   #       Traveltime_empcent = as.numeric(Traveltime_empcent),
          No_car_rate = as.numeric(No_car_rate),
@@ -69,9 +62,9 @@ MANCH_dataset_descript <- read_csv("Data/MANCH_dataset_full.csv") %>%
          Single_parent_household_rate = as.numeric(Single_parent_household_rate),
          Low_qual_percent = as.numeric(Low_qual_percent),
          Townsuburb = as.factor(Townsuburb),
-        TownNamed = as.factor(TCITY15),
-         Man_Pop = as.numeric(Man_Pop),
-         Man_Pop_Dens = as.numeric(Man_Pop_Dens),
+         TownNamed = as.factor(TCITY15),
+         LCR_Pop = as.numeric(LCR_Pop),
+         LCR_Pop_Dens = as.numeric(LCR_Pop_Dens),
          SEC_Management_pc = as.numeric(SEC_Management_pc))
 #         SEC_Lower_supervisory_routine_pc = as.numeric(SEC_Lower_supervisory_routine_pc))
 
@@ -91,7 +84,7 @@ Employment <-read_csv("Data/census2021-ts066/census2021-ts066-lsoa.csv") %>%
 
 
 # Descriptive Table
-(Descrp_table <- (MANCH_dataset_descript %>% 
+(Descrp_table <- (LCR_dataset_descript %>% 
                                  dplyr::select(-c("LSOA_Code", 
                                            "LSOA_Name", 
                                            "LAD22CD",
@@ -101,8 +94,8 @@ Employment <-read_csv("Data/census2021-ts066/census2021-ts066-lsoa.csv") %>%
    tbl_summary(
     statistic = list(all_continuous() ~ "{mean} ({sd})",
                                all_categorical() ~ "{n} / {N} ({p}%)"),
-        label = list(      Man_Pop ~ "Population",
-                           Man_Pop_Dens ~ "Population Density (persons / sq.km)",
+        label = list(      LCR_Pop ~ "Population",
+                           LCR_Pop_Dens ~ "Population Density (persons / sq.km)",
                       #     TravelTime_Jobcentre ~ "Travel Time to Jobcentre (mins)",
                       #     Traveltime_empcent ~ "Travel Time to Employment Centre (mins)",
                            PT_Job_Access_Index ~ "PTJA Index (/1,000)",
@@ -118,7 +111,7 @@ Employment <-read_csv("Data/census2021-ts066/census2021-ts066-lsoa.csv") %>%
 #  add_overall() %>%
   modify_header(label ~ "Variable" )%>%
 #  italicize_levels() %>%
-#  modify_caption("Table 1. Summary Statistics for Greater Manchester Combined Authority (GMCA) at Lower Super Output Area (LSOA) Level") %>%
+#  modify_caption("Table 1. Summary Statistics for Liverpool City Region Combined Authority (LCR) at Lower Super Output Area (LSOA) Level") %>%
   modify_footnote(everything() ~ NA) ) %>%
   modify_header(stat_0 ~ "Mean (Inter-quartile Range)") %>%
     
@@ -132,58 +125,74 @@ doc <- read_docx()
 # Add the flextable to the document
 doc <- body_add_flextable(doc, value = Descrp_table)
 # Save the Word document
-print(doc, target = "../Final_Report/descriptive_table.docx")
+print(doc, target = "descriptive_table_LCR.docx")
 
 # Calculate higher-level unemployment rates
 Overall_unemp_rate <- round((sum(Employment$Unemployed) / sum(Employment$"Economically_active") *100),2)
-GMCA_unemployment_rate <- round((sum(MANCH_dataset_descript$Unemployed) / sum(MANCH_dataset_descript$"Economically_active") *100),2)
-Town_unemp_rate <- round((MANCH_dataset_descript %>%
+LCR_unemployment_rate <- round((sum(LCR_dataset_descript$Unemployed) / sum(LCR_dataset_descript$"Economically_active") *100),2)
+Town_unemp_rate <- round((LCR_dataset_descript %>%
   filter(Townsuburb == "Urban") %>%
   summarise(Unemployed_sum = sum(Unemployed),
             Economically_active_sum = sum(Economically_active)) %>%
   mutate(Unemployment_rate = (Unemployed_sum / Economically_active_sum) * 100) %>%
   pull(Unemployment_rate)),2)
-Suburb_unemp_rate <- round((MANCH_dataset_descript %>%
+Suburb_unemp_rate <- round((LCR_dataset_descript %>%
   filter(Townsuburb == "Suburb") %>%
   summarise(Unemployed_sum = sum(Unemployed),
             Economically_active_sum = sum(Economically_active)) %>%
   mutate(Unemployment_rate = (Unemployed_sum / Economically_active_sum) * 100) %>%
   pull(Unemployment_rate)),2)
-GMCA_jobs <- sum(MANCH_dataset_descript$Employed_Population)
-GMCA_pop <- sum(MANCH_dataset_descript$Economically_active)
-GMCA_urban_pop <- sum(MANCH_dataset_descript %>%
+LCR_jobs <- sum(LCR_dataset_descript$Employed_Population)
+LCR_population <- sum(LCR_dataset_descript$Economically_active)
+LCR_urban_pop <- sum(LCR_dataset_descript %>%
   filter(Townsuburb == "Urban") %>%
   select(Economically_active))
-GMCA_suburb_pop <- sum(MANCH_dataset_descript %>% 
+LCR_suburb_pop <- sum(LCR_dataset_descript %>%
   filter(Townsuburb == "Suburb") %>%
   select(Economically_active))
-GMCA_urban_jobs <- sum(MANCH_dataset_descript %>%
+LCR_urban_jobs <- sum(LCR_dataset_descript %>%
   filter(Townsuburb == "Urban") %>%
   select(Employed_Population))
-GMCA_suburb_jobs <- sum(MANCH_dataset_descript %>%
+LCR_suburb_jobs <- sum(LCR_dataset_descript %>%
   filter(Townsuburb == "Suburb") %>%
   select(Employed_Population))
 # Create a data frame with the additional rows
 additional_rows <- data.frame(
-  TownNamed = c("GMCA", "GMCA Urban Areas", "GMCA Suburban Areas", "England and Wales"),
-  Jobs = c(GMCA_jobs, GMCA_urban_jobs, GMCA_suburb_jobs, NA),
-  Population = c(GMCA_pop, GMCA_urban_pop, GMCA_suburb_pop, NA),
-  Jobs_per_person = c(round((GMCA_jobs/GMCA_pop),2), 
-                      round((GMCA_urban_jobs/GMCA_urban_pop),2), 
-                      round((GMCA_suburb_jobs/GMCA_suburb_pop),2), NA),
-  Unemployment_Rate_Town = c(GMCA_unemployment_rate, Town_unemp_rate, Suburb_unemp_rate, Overall_unemp_rate)) %>%
-  arrange(match(TownNamed, c("GMCA Urban Areas","GMCA Suburban Areas","GMCA","England and Wales")))
+                              TownNamed = c("LCR",
+                                            "LCR Urban Areas",
+                                            "LCR Suburban Areas",
+                                            "England and Wales"),
+                              Jobs = c(LCR_jobs,
+                                       LCR_urban_jobs,
+                                       LCR_suburb_jobs,
+                                       NA),
+                              Population = c(LCR_population,
+                                             LCR_urban_pop,
+                                             LCR_suburb_pop,
+                                             NA),
+                              Jobs_per_person = c(round((LCR_jobs / LCR_pop), 2),
+                                                  round((LCR_urban_jobs/LCR_urban_pop), 2),
+                                                  round((LCR_suburb_jobs/LCR_suburb_pop), 2),
+                                                  NA),
+                              Unemployment_Rate_Town = c(LCR_unemployment_rate,
+                                                         Town_unemp_rate,
+                                                         Suburb_unemp_rate,
+                                                         Overall_unemp_rate)) %>%
+  arrange(match(TownNamed, c("LCR Urban Areas",
+                              "LCR Suburban Areas",
+                              "LCR",
+                              "England and Wales")))
 
 # Cities and Towns Table
-towns_jobs <- MANCH_dataset_descript  %>%
+towns_jobs <- LCR_dataset_descript  %>%
   dplyr::select("TownNamed", 
                 "Employed_Population",
-                "Man_Pop",
+                "LCR_Pop",
                 "Unemployed",
                 "Economically_active") %>%
   group_by(TownNamed) %>%
   summarise(Jobs = sum(Employed_Population), 
-            Population = sum(Man_Pop),
+            Population = sum(LCR_Pop),
             Jobs_per_person = round((Jobs/Population), 2),
             Unemployment_Rate_Town = round((sum(Unemployed)/sum(Economically_active)*100),2)) %>%
   filter(TownNamed != "Suburb") %>%
@@ -197,11 +206,11 @@ towns_jobs <- MANCH_dataset_descript  %>%
   hline(i=c(8,10,11)) %>%
   set_table_properties(layout = "autofit",
                        align = "center") %>%
-  set_caption("Table 2. Employment and Population in Greater Manchester Combined Authority (GMCA) Cities and Towns") 
+  set_caption("Table 2. Employment and Population in Liverpool City Region Combined Authority (LCRCA) Cities and Towns") 
 
 # Create a Word document
 doc <- read_docx()
 # Add the flextable to the document
 doc <- body_add_flextable(doc, value = towns_jobs)
 # Save the Word document
-print(doc, target = "../Final_Report/towns_jobs.docx")
+print(doc, target = "towns_jobs_LCR.docx")
